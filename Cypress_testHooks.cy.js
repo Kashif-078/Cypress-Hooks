42cda0ec15952238db587dfb0051ab1c367cc512
Kashif Shabbir <135126946+Kashif-078@users.noreply.github.com>
2023-06-01 17:20:58 +0500

    Add files via upload

---
+++ Cypress_testHooks.cy.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+describe('my suite', () =>
+{
+    // This hook runs once before all the tests in a test suite
+    before(() => 
+    {
+        cy.log('before hook Executed')
+    })
+  
+    //  This hook runs before each individual test within a test suite
+    beforeEach(() => 
+    {
+        cy.log('beforeEach hook Executed')
+    })
+
+    // This hook runs after each individual test within a test suite. It can be used 
+    afterEach(() => 
+    {
+        cy.log('afterEach hook Executed')
+    })
+
+    // Tis hook runs once after all the tests in a test suite have completed
+    after(() => 
+    {
+        cy.log('after hook Executed')
+    })
+
+    // To group a set of related tests
+    context(() =>
+    {
+        it('Test 1', () => 
+        {
+            cy.log('Test Case 1')
+        })
+        
+        it('Test 2', () => 
+        {
+            cy.log('Test Case 2')
+        })
+    })
+})
